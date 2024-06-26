4e889941c2704173bdb3aeea4c1c9056b65cb12c
Reginald Chand <reginald-chand@outlook.com>
2024-06-27 03:33:45 +1200

    Add postId validation

--- server/validators/like/like.controller.validator.mjs
+++ server/validators/like/like.controller.validator.mjs
@@ -4,7 +4,9 @@ export const likeControllerValidator = Joi.object({
   _csrf: Joi.string().required(),
 
   userName: Joi.string().pattern(new RegExp("^[a-z]+$")).required(),
-  postId: Joi.string().required(),
+  postId: Joi.string()
+    .pattern(new RegExp("^(?:[0-9a-fA-F]{24}|(?:[0-9]{1,19}))$"))
+    .required(),
 
   userData: Joi.object().required(),
 });

aee1b7ad828583c5bd0f43dfcb2a2e4f497b48de
Reginald Chand <reginald-chand@outlook.com>
2024-06-30 12:23:59 +1200

    Update csrf token name

--- server/validators/like/like.controller.validator.mjs
+++ server/validators/like/like.controller.validator.mjs
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const likeControllerValidator = Joi.object({
-  _csrf: Joi.string().required(),
+  csrfToken: Joi.string().required(),
 
   userName: Joi.string().pattern(new RegExp("^[a-z]+$")).required(),
   postId: Joi.string()
